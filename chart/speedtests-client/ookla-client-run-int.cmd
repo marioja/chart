723bb47dd6efed13d79b8a49d3a4fe768ab00875
Mario Jauvin <mfj@earthling.net>
2020-10-05 13:09:18 -0400

    CamelCase headers

--- chart/speedtests-client/ookla-client-run-int.cmd
+++ chart/speedtests-client/ookla-client-run-int.cmd
@@ -4,9 +4,9 @@ set folder=%~dp0ookla
 mkdir "%folder%" 2>nul
 set logfile=%folder%\ookla-client.log
 set csvfile=%folder%\ookla-client.csv
-set header="Date","serverName","serverId","latency","jitter","packetLoss","download","upload","downloadBytes","uploadBytes","shareUrl"
+set header="Date","ServerName","ServerId","Latency","Jitter","PacketLoss","Download","Upload","DownloadBytes","UploadBytes","ShareUrl"
 if not exist "%csvfile%" echo %header% >"%csvfile%"
-<nul set /p="%date% %time%", >>"%csvfile%"
+<nul set /p=""%date% %time%"," >>"%csvfile%"
 "%~dp0speedtest" -f csv --progress=no >>"%csvfile%"
 "%~dp0dbxcli-windows-amd64.exe" put "%csvfile%" "/MFJ Shared/ookla-client.csv"
 copy "%csvfile%" "c:\inetpub\wwwroot_public\chart"
