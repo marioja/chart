47906e60b00a8558320ab4280c07dff20ffcf140
Mario Jauvin <mfj@earthling.net>
2020-10-05 09:44:46 -0400

    add timestamp to csv file

--- chart/speedtests-client/ookla-client-run-int.cmd
+++ chart/speedtests-client/ookla-client-run-int.cmd
@@ -6,7 +6,7 @@ set logfile=%folder%\ookla-client.log
 set csvfile=%folder%\ookla-client.csv
 set header="Date","serverName","serverId","latency","jitter","packetLoss","download","upload","downloadBytes","uploadBytes","shareUrl"
 if not exist "%csvfile%" echo %header% >"%csvfile%"
-<nul set /p=%date% %time% >>"%logfile%"
+<nul set /p="%date% %time%", >>"%csvfile%"
 "%~dp0speedtest" -f csv --progress=no >>"%csvfile%"
 "%~dp0dbxcli-windows-amd64.exe" put "%csvfile%" "/MFJ Shared/ookla-client.csv"
 copy "%csvfile%" "c:\inetpub\wwwroot_public\chart"
